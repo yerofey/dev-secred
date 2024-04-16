f5c624617e8b9ce460303e2ecc8e9fbb53c80b23
Yerofey S. <pm@yerofey.dev>
2024-04-16 12:32:23 +0200

    Add base_mainnet network configuration

--- hardhat-project/hardhat.config.cjs
+++ hardhat-project/hardhat.config.cjs
@@ -22,5 +22,17 @@ module.exports = {
         'ee677f4126ba47f75e5dcd987cc13002ab9cf1216bb387331824b92dc208da83',
       ],
     },
+    base_mainnet: {
+      url: 'https://shy-red-dinghy.base-mainnet.quiknode.pro/e60e1277d62a9730a3731f29e69274871b424f9b/',
+      accounts: [
+        'ee677f4126ba47f75e5dcd987cc13002ab9cf1216bb387331824b92dc208da83',
+      ],
+    },
+    // base_sepolia: {
+    //   url: 'https://proportionate-long-borough.base-sepolia.quiknode.pro/2f7250c6c4daf02de4b40f8b88b073af7f54ebc7/',
+    //   accounts: [
+    //     'ee677f4126ba47f75e5dcd987cc13002ab9cf1216bb387331824b92dc208da83',
+    //   ],
+    // },
   },
 };
